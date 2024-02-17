01c9ee97492474547ab14f78180f1400d1ef28b5
edgar <edgardavid1503@gmail.com>
2024-02-16 20:19:39 -0800

    corrected an error

---
+++ src/app/dashboard/goals/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import DashboardSkeleton from '@/app/ui/skeletons';
+import Footer from '@/app/ui/footer';
+import { GoalComponent } from '@/app/ui/goals/goalComponent';
+export default function Page({ params }: { params: { id: string } }) {
+  const { data: session } = useSession();
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session) {
+        setUserEmail(session.user?.email || null);
+        setLoading(false); // Set loading to false once the user email is set
+      } else {
+        router.push("/");
+      }
+    };
+
+    fetchData();
+  }, [session, router]);
+
+  if (loading) {
+    return <DashboardSkeleton />; 
+  }
+
+  if (userEmail) {
+    return (
+      <div className='flex flex-col m-5'>
+        <GoalComponent params={params} />
+        <Footer/>
+
+      </div>
+    );
+  }
+
+  return null; // You might want to handle the case when userEmail is null differently
+}
