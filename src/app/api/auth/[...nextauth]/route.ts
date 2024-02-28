64213c69a28a1443da505531b7c5ad2e450edc51
edgar <edgardavid1503@gmail.com>
2024-02-27 18:05:25 -0800

    corrected error with facebook login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
-import FacebookProvider from "next-auth/providers/apple";
+import FacebookProvider from "next-auth/providers/facebook";
 
 const handler = NextAuth({
   providers: [
